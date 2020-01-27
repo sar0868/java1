c0a8b261ecb4a75869e96c0d08914721d7a62613
sar0868 <safarov-ar@yandex.ru>
2020-01-27 14:40:50 +0300

    lesson 4 27.01

--- src/java/lesson4/Main.java
+++ src/java/lesson4/Main.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class Main {
 
     static void showTable(char [][] t) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
                 System.out.print("|" + t[i][j]);
             }
             System.out.println("|");
@@ -17,14 +17,14 @@ public class Main {
     }
 
     static void checkVictory(int [][] X, boolean user) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             int sX = 0, sY = 0, dS1 = 0, dS2 = 0;
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < 5; j++) {
                 sX += X[i][j];
                 sY += X[j][i];
                 dS1 += X[j][j];
-                dS2 += X[j][2 - j];
-                if (sX == 3 || sY == 3 || dS1 == 3 || dS2 == 3) {
+                dS2 += X[j][4 - j];
+                if (sX == 4 || sY == 4 || dS1 == 4 || dS2 == 4) { //надо 4 подряд
                     System.out.println(user ? "Вы победили!" : "Вы проиграли");
                     //System.out.println(sX + " " + sY + " " + dS1 + " " + dS2);
                     System.exit(0);
@@ -36,21 +36,26 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner in = new Scanner(System.in);
         char def = '_';
-        char [][] t = new char[3][3];
-        int [][] X = new int[3][3];
-        int [][] o = new int[3][3];
-        for (int i = 0; i < 3; i++) {
+        char [][] t = new char[5][5];
+        int [][] X = new int[5][5];
+        int [][] o = new int[5][5];
+        for (int i = 0; i < 5; i++) {
             Arrays.fill(t[i], def);
         }
         showTable(t);
         System.out.println("Вы играете крестиками!" +
                 " Выберете номер строки и столбца");
         char user = 'X';
-        int limit = 9;
+        int limit = 25;
         while (true) {
             System.out.println("Ваш ход:");
             int x = in.nextInt(), y = in.nextInt();
             x--; y--;
+            while (x > t.length || y > t.length){
+                System.out.println("Такой клетки нет! Ваш ход:");
+                x = in.nextInt(); x--;
+                y = in.nextInt(); y--;
+            }
             while (t[x][y] != def) {
                 System.out.println("Выберите пустую клетку! Ваш ход:");
                 x = in.nextInt(); x--;
@@ -80,8 +85,8 @@ public class Main {
 
     private static void moveAI(char[][] t, int [][] X) {
         ArrayList<int []> pairs = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
                 if (t[i][j] == '_') {
                     pairs.add(new int[]{i, j});
                 }
