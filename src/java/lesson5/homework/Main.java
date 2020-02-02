db56b8a06fdf87df2611f0d0de8a5c9c28c2066b
ALeksey Safarov <safarov-ar@yandex.ru>
2020-02-02 16:44:18 +0300

    Revert "lesson 6"
    This reverts commit 33efd388

--- src/java/lesson5/homework/Main.java
+++ src/java/lesson5/homework/Main.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-////        HomeWork.Person pers = new HomeWork.Person("Ivan Ivanov", "Engineer",
-////                "ivivan@mailbox.com", "892312312", 30001, 30);
-//        HomeWork.Person[] persAll = new HomeWork.Person[5];
-//        persAll = HomeWork.createFivePerson();
-//        ArrayList<HomeWork.Person> pers = new ArrayList<>();
-//        pers = HomeWork.filterBiggerThan40(persAll);
-//        for (HomeWork.Person men: pers) {
-//            System.out.println(men);
-//        }
+//        HomeWork.Person pers = new HomeWork.Person("Ivan Ivanov", "Engineer",
+//                "ivivan@mailbox.com", "892312312", 30001, 30);
+        HomeWork.Person[] persAll = new HomeWork.Person[5];
+        persAll = HomeWork.createFivePerson();
+        ArrayList<HomeWork.Person> pers = new ArrayList<>();
+        pers = HomeWork.filterBiggerThan40(persAll);
+        for (HomeWork.Person men: pers) {
+            System.out.println(men);
+        }
     }
 }
