2e4d0fc77d9e2ca0061cdc1aee564a2380fb14fe
Mikhail <levinmk23@gmail.com>
2020-02-04 20:49:30 +0300

    Lesson 7. Java 1.

---
+++ src/java/lesson6/homework/animal2/Jump.java
@@ -0,0 +1,5 @@
+package lesson6.homework.animal2;
+
+public interface Jump extends Task {
+    void doing(int limit);
+}
---
+++ src/java/lesson6/homework/animal2/Task.java
@@ -0,0 +1,5 @@
+package lesson6.homework.animal2;
+
+public interface Task  {
+    void doing(int limit);
+}
